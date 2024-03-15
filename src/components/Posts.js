757ce33c7248dd00b29ef990003a6f36fb7ef303
Mike Setzer <michaelsetzerucf@gmail.com>
2024-03-15 16:16:33 -0400

    48

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions';
 
 class Posts extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: []
-        }
-    }
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(res => res.json())
-            .then(data => this.setState({posts: data}));
-    }
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
         <div key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.body}</p>
@@ -21,11 +16,11 @@ class Posts extends Component {
     ));
     return (
       <div>
-        <h2>Posts</h2>
+        <h1>Posts</h1>
         {postItems}
       </div>
     )
   }
 }
 
-export default Posts
+export default connect(null, { fetchPosts })(Posts);
